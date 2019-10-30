9afd8b86c11d400422b58ba098f8a4d4aafa6f1e
Sarah <sam323@lehigh.edu>
2019-10-30 16:12:08 -0400

    Added my resume

---
+++ AboutMe.R
@@ -0,0 +1,15 @@
+## Sarah McCall
+## Seattle WA
+## Work Experience
+## 19 months at a Coffee Shop working as a barista and busser
+## Lacrosse Referee for 2 years during the lacrosse season
+## Other experience
+## Lacrosse: Varsity for 4 years, captain for 1
+## Seattle South African Scholarship Foundation, coordinating fundraisers
+##Education
+## University Prep, graduated 2019
+## Lehigh University, College of Business and Economics, first year
+
+## I would like to note that this is not my actual resume.
+## My actual resume is much prettier, but it is not a word document
+## So, I cannot upload it. I hope this is acceptable.
